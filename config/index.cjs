212a17f0820f72237d1d479ac58afd8dd43f39fa
lezhengsiyuan <120650165+lezhengsiyuan@users.noreply.github.com>
2023-02-18 12:04:09 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,11 +7,13 @@
  */
 const USER_CONFIG = {
 
+    USE_PASSAGE: 'push-plus',
+  
   // 使用微信测试号：公众号APP_ID
-  APP_ID: 'wx9b5955ee65fd8ec3',
+  //APP_ID: 'wx9b5955ee65fd8ec3',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '0dff83c6a572a594d3dc4b1799ac577a',
+  //APP_SECRET: '0dff83c6a572a594d3dc4b1799ac577a',
 
   PROVINCE: '上海',
   CITY: '上海',
@@ -21,9 +23,10 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ozoQg5tPNpTFdx1n04j0jLeqeqMg',
+      //id: 'ozoQg5tPNpTFdx1n04j0jLeqeqMg',
+      id: '53f1045ec4574a13aa6f4e442db39ae7',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'fOaOxIlaTJZ36tjRBGZJojIaJ888N4vDZZ-otAql7XU',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-22',
       festivals: [
